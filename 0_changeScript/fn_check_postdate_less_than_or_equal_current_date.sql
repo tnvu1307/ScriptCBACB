92e212a14eff3b4e191d415ed11c62a36a6dedb6
Trương Nhật Vy <vytruongjb@gmail.com>
2024-02-01 09:51:11 +0700

    fix: upload function

---
+++ 0_changeScript/fn_check_postdate_less_than_or_equal_current_date.sql
@@ -0,0 +1,17 @@
+SET DEFINE OFF;
+CREATE OR REPLACE function fn_check_postdate_less_than_or_equal_current_date(pv_postdate in varchar2) return number
+    is
+    v_postdate date;
+    v_currentDate date;
+begin
+    v_postdate := TO_DATE(pv_postdate,'DD/MM/YYYY');
+    v_currentDate := GETCURRDATE;
+    
+    if v_postdate > v_currentDate
+    then
+        return -1;
+    end if;
+    
+    return 0;
+end;
+/
