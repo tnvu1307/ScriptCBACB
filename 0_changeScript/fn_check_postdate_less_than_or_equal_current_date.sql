7cf746815b8108f491afc5c238c881bfc04be658
Trương Nhật Vy <vytruongjb@gmail.com>
2024-02-01 09:44:17 +0700

    refactor: change name function `fn_6688_check_postdate_less_than_or_equal_current_Date`

--- 0_changeScript/fn_check_postdate_less_than_or_equal_current_date.sql
+++ 0_changeScript/fn_check_postdate_less_than_or_equal_current_date.sql
@@ -1,5 +1,5 @@
 SET DEFINE OFF;
-CREATE OR REPLACE function fn_6688_check_postdate_less_than_or_equal_current_Date(pv_postdate in varchar2) return number
+CREATE OR REPLACE function fn_check_postdate_less_than_or_equal_current_date(pv_postdate in varchar2) return number
     is
     v_postdate date;
     v_currentDate date;
