73227e3d19f01d909c52667fefe1e9ed46062a8f
Trương Nhật Vy <vytruongjb@gmail.com>
2024-02-26 16:05:55 +0700

    update headoffice and address

--- 2_parameter/SYSVAR/SYSVAR.BRADDRESS_EN.sql
+++ 2_parameter/SYSVAR/SYSVAR.BRADDRESS_EN.sql
@@ -1,1 +1,9 @@
-SET DEFINE OFF;DELETE FROM SYSVAR WHERE 1 = 1 AND NVL(VARNAME,'NULL') = NVL('BRADDRESS_EN','NULL');Insert into SYSVAR   (GRNAME, VARNAME, VARVALUE, VARDESC, EN_VARDESC, EDITALLOW, DATATYPE) Values   ('SYSTEM', 'BRADDRESS_EN', 'Ground Floor, Mezzanine, Floor 2, 3, 11, 15 of Empress Tower, 138-142 Hai Ba Trung, Da Kao Ward, District 1, Ho Chi Minh City, Vietnam', 'Địa chỉ', 'Address', 'Y', 'C');COMMIT;
+SET DEFINE OFF;
+
+DELETE FROM SYSVAR WHERE 1 = 1 AND NVL(VARNAME,'NULL') = NVL('BRADDRESS_EN','NULL');
+
+Insert into SYSVAR
+   (GRNAME, VARNAME, VARVALUE, VARDESC, EN_VARDESC, EDITALLOW, DATATYPE)
+ Values
+   ('SYSTEM', 'BRADDRESS_EN', '442, Nguyen Thi Minh Khai street, Ward 5, District 3, Ho Chi Minh City, Vietnam', 'Địa chỉ', 'Address', 'Y', 'C');
+COMMIT;
