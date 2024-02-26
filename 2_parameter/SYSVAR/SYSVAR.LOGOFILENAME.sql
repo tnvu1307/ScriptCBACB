292a84a1b15f1fd59b1e0abf48e2b4d7b74f59a5
Trương Nhật Vy <vytruongjb@gmail.com>
2024-02-26 16:35:06 +0700

    cap nhat lai ten file cua logo report

--- 2_parameter/SYSVAR/SYSVAR.LOGOFILENAME.sql
+++ 2_parameter/SYSVAR/SYSVAR.LOGOFILENAME.sql
@@ -5,5 +5,6 @@ DELETE FROM SYSVAR WHERE 1 = 1 AND NVL(VARNAME,'NULL') = NVL('LOGOFILENAME','NUL
 Insert into SYSVAR
    (GRNAME, VARNAME, VARVALUE, VARDESC, EN_VARDESC, EDITALLOW, DATATYPE)
  Values
-   ('SYSTEM', 'LOGOFILENAME', 'shinhanreport.jpg', 'Logo bao cao', NULL, 'N', 'C');
+   ('SYSTEM', 'LOGOFILENAME', 'acbreport.jpg', 'Logo bao cao', NULL, 'N', 'C');
 COMMIT;
+
