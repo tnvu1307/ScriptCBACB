02f53929450098d6011404815167696b525032a5
Trương Nhật Vy <vytruongjb@gmail.com>
2024-03-11 10:09:30 +0700

    Chỉnh sửa câu search hiện tại thành câu search trên Pro

--- 2_parameter/SEARCH/SEARCH.HOLDORDER.sql
+++ 2_parameter/SEARCH/SEARCH.HOLDORDER.sql
@@ -1,24 +1,41 @@
-SET DEFINE OFF;DELETE FROM SEARCH WHERE 1 = 1 AND NVL(SEARCHCODE,'NULL') = NVL('HOLDORDER','NULL');Insert into SEARCH   (SEARCHCODE, SEARCHTITLE, EN_SEARCHTITLE, SEARCHCMDSQL, OBJNAME, FRMNAME, ORDERBYCMDSQL, TLTXCD, CNTRECORD, ROWPERPAGE, AUTOSEARCH, INTERVAL, AUTHCODE, ROWLIMIT, CMDTYPE, CONDDEFFLD, BANKINQ, BANKACCT) Values   ('HOLDORDER', 'Hold số tiền mua khớp', 'Hold the matched purchase amount', 'SELECT STS.AUTOID REQTXNUM, ''HOLDOD'' REQCODE, CF.CUSTID ACCTNO, CF.CIFID, CF.CUSTODYCD, CF.FULLNAME CUSTNAME, 
-    DD.ACCTNO DDACCTNO, DD.REFCASAACCT, DD.BALANCE, DD.HOLDBALANCE, DD.HOLDBALANCE BANKHOLDEDBYBROKER,
-    SB.CODEID, SB.SYMBOL, 
-    STS.TXDATE, STS.CLEARDATE SETTLE_DATE, STS.RECEVING QUANTITY, STS.CASHNETTING AMOUNT, NVL(STS.FEEAMT,0) FEE, NVL(STS.VAT,0) TAX
-FROM CFMAST CF, SBSECURITIES SB,
-(SELECT * FROM SYSVAR WHERE VARNAME = ''DEALINGCUSTODYCD'') SYS,
-(SELECT * FROM STSCHD_NETOFF WHERE ISHOLD = ''N'' AND DELTD = ''N'') STS,
-(SELECT * FROM DDMAST WHERE STATUS NOT IN (''C'') AND CCYCD = ''VND'' AND ISDEFAULT = ''Y'') DD
-WHERE CF.CUSTID = STS.CUSTID
-AND CF.CUSTID = DD.CUSTID
-AND STS.CODEID = SB.CODEID
-AND STS.ISHOLD = ''N''
-AND STS.CASHNETTING > 0
-AND SUBSTR(CF.CUSTODYCD,0,4) NOT LIKE SYS.VARVALUE
-AND NOT EXISTS (
-    SELECT F.CVALUE 
-    FROM TLLOG TL, TLLOGFLD F 
-    WHERE TL.TXNUM = F.TXNUM 
-    AND TL.TXDATE = F.TXDATE 
-    AND TL.TLTXCD = ''6690'' 
-    AND F.FLDCD = ''95'' 
-    AND TL.TXSTATUS IN(''1'', ''4'') 
-    AND F.CVALUE =  TO_CHAR(STS.AUTOID)
-)', 'HOLDORDER', NULL, NULL, '6690', NULL, 5000, 'N', 1, 'NNNNYYYNNN', 'Y', 'T', NULL, 'N', NULL);COMMIT;
+SET DEFINE OFF;
+
+DELETE FROM SEARCH WHERE 1 = 1 AND NVL(SEARCHCODE,'NULL') = NVL('HOLDORDER','NULL');
+
+Insert into SEARCH
+   (SEARCHCODE, SEARCHTITLE, EN_SEARCHTITLE, SEARCHCMDSQL, OBJNAME, FRMNAME, ORDERBYCMDSQL, TLTXCD, CNTRECORD, ROWPERPAGE, AUTOSEARCH, INTERVAL, AUTHCODE, ROWLIMIT, CMDTYPE, CONDDEFFLD, BANKINQ, BANKACCT)
+ Values
+   ('HOLDORDER', 'Hold số tiền mua khớp', 'Hold the matched purchase amount', 'SELECT  OD.ORDERID,''HOLDOD'' REQCODE,SB.CODEID , SB.SYMBOL,CF.CUSTID ACCTNO, DD.ACCTNO DDACCTNO,DD.REFCASAACCT ,
+                                         CF.CUSTODYCD,CF.FULLNAME CUSTNAME, A1.<@CDCONTENT> EXECTYPE,
+                                         OD.TXDATE,OD.CLEARDATE SETTLE_DATE,
+                                         (CASE WHEN CF.FEEDAILY = ''Y'' THEN OD.NETAMOUNT ELSE OD.EXECAMT END) AMOUNT, OD.EXECQTTY QUANTITY,NVL(OD.FEEAMT,0) FEE,NVL(OD.TAXAMT,0) TAX,FA.SHORTNAME,
+                                         CF.CIFID,DD.BALANCE,DD.HOLDBALANCE,DD.HOLDBALANCE BANKHOLDEDBYBROKER,FA.AUTOID MEMBERID,
+                                         VW_MBR.DISPLAY BRNAME,VW_MP.DISPLAY BRPHONE
+                             FROM TLLOG TL, ODMAST OD,DDMAST DD,CFMAST CF,SBSECURITIES SB,FAMEMBERS FA,VW_CUSTODYCD_MEMBER MM,
+                                 VW_MEMBER_BROKER VW_MBR,VW_MEMBER_PHONE VW_MP,ALLCODE A1,
+                                 (select varvalue  from sysvar where varname = ''DEALINGCUSTODYCD'')sys
+                                 WHERE   TL.TXNUM = OD.TXNUM
+                                     AND OD.ORSTATUS = ''4''
+                                     AND TL.TLTXCD = ''8893''
+                                     AND DD.STATUS <> ''C'' AND DD.CCYCD = ''VND'' AND DD.ISDEFAULT = ''Y''
+                                     AND OD.CUSTODYCD = CF.CUSTODYCD
+                                     AND CF.CUSTODYCD = DD.CUSTODYCD
+                                     AND OD.CODEID = SB.CODEID
+                                     AND FA.AUTOID = OD.MEMBER
+                                     AND MM.VALUE = FA.AUTOID
+                                     AND MM.FILTERCD = OD.CUSTODYCD
+                                     AND OD.EXECTYPE = ''NB''
+                                     AND OD.ISHOLD = ''N''
+                                     AND FA.AUTOID = VW_MBR.FILTERCD (+)
+                                     AND MM.value = VW_MBR.value (+)
+                                     AND FA.AUTOID = VW_MP.FILTERCD (+)
+                                     AND MM.value = VW_MP.value (+)
+                                     AND FA.ROLES = ''BRK''
+                                     AND OD.DELTD <> ''Y''
+                                     AND A1.CDNAME = ''EXECTYPE'' AND A1.CDTYPE = ''OD'' AND A1.CDVAL = ''NB'' AND A1.CDVAL  = OD.exectype
+                                     and SUBSTR(cf.custodycd,0,4) not like sys.varvalue
+                                     AND NOT EXISTS ( SELECT F.CVALUE FROM
+                                         TLLOG TL, TLLOGFLD F WHERE TL.TXNUM = F.TXNUM AND
+                                         TL.TXDATE = F.TXDATE AND TL.TLTXCD = ''6690'' AND F.FLDCD
+                                         = ''95'' AND TL.TXSTATUS IN(''1'', ''4'') AND F.CVALUE =  OD.ORDERID)', 'HOLDORDER', NULL, NULL, '6690', NULL, 5000, 'N', 1, 'NNNNYYYNNN', 'Y', 'T', NULL, 'N', NULL);
+COMMIT;
